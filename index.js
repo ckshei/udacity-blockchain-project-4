33dd798dae16497657c41d202fe664e092fe2e1b
Shei <shei@Sheis-MacBook.local>
2019-03-27 13:29:45 -0700

    handling errors + no block data

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/* ===== Executable Test ==================================
-|  Use this file to test your project.
-|  =========================================================*/
-
 const BlockChain = require('./BlockChain.js');
 const Block = require('./Block.js');
 
@@ -12,8 +8,16 @@ const myBlockchain = new BlockChain.Blockchain()
 
 app.get('/', (req,res) => res.send("hello world!"))
 
-app.get('/block/:blockheight', (req,res) => myBlockchain.getBlock(req.params.blockheight).then(block => res.send(block)));
+app.get('/block/:blockheight', (req,res) => myBlockchain.getBlock(req.params.blockheight).then(block => res.send(block), err => res.send(err)));
 
-app.post('/block', (req, res) => myBlockchain.addBlock(req.params.body).then(block => res.send(block)));
-
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.post('/block', (req, res) => {
+    let data = req.params.body
+    if (data.body.length > 0) {
+        myBlockchain.addBlock(req.params.body).then(block => res.send(block), err => res.send(err))
+    } else {
+        res.send({
+            status: 'error',
+            message: 'block data is missing'
+        })
+    }
+});
